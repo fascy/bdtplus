35db69b6a5fe60b4e601ee00adb3e53f396ae6e5
fascy <fascy97>
2024-07-17 14:07:46 +0800

    ip

---
+++ ip_tool.py
@@ -0,0 +1,48 @@
+nodes = '''
+i-0289ea432c84c0be0 	3.86.244.107 	172.31.88.40
+i-0880c827eb1b01eb5 	3.141.13.254 	172.31.27.67
+i-061d489292cc90cf4 	54.193.50.204 	172.31.12.144
+i-095c636146a4309f7 	54.149.245.140 	172.31.36.250 
+'''
+
+num_regions = 16
+N = 16
+
+n = int(N / num_regions)
+r = N - num_regions * n
+
+each_region_n = []
+for i in range(num_regions):
+    if r > 0:
+        each_region_n.append(n + 1)
+        r -= 1
+    else:
+        each_region_n.append(n)
+print(each_region_n)
+
+public_ips = []
+private_ips = []
+
+for line in nodes.splitlines():
+    try:
+        _, public, private = line.split()
+        public_ips.append(public)
+        private_ips.append(private)
+    except:
+        pass
+
+print("N=%d" % len(public_ips))
+
+print("# public IPs")
+print("pubIPsVar=(", end='')
+for i in range(len(public_ips) - 1):
+    print("[%d]=\'%s\'" % (i, public_ips[i]))
+i = len(public_ips) - 1
+print("[%d]=\'%s\')" % (i, public_ips[i]))
+
+print("# private IPs")
+print("priIPsVar=(", end='')
+for i in range(len(private_ips) - 1):
+    print("[%d]=\'%s\'" % (i, private_ips[i]))
+i = len(private_ips) - 1
+print("[%d]=\'%s\')" % (i, private_ips[i]))
